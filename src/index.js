cb552147ed0a86bb5458d6b91bda26ab427120ba
shiramashiro <zhengrenfu@outlook.com>
2022-05-09 20:06:24 +0800

    区域呈现卡片样式

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 import "./index.less";
-import options from "@consts/options";
+import options from "./consts/options";
 import {
     isPostPage,
     showSidebar,
-    clearElement,
-    addElement,
-} from "@consts/tools";
-import buildGithubCorner from "@modules/githubCorner";
-import buildCustomHeader from "@modules/customHeader";
-import buildCustomFooter from "@modules/customFooter";
-import buildProfile from "@modules/profile";
-import buildPostCatalog from "@modules/postCatalog";
-import buildPostLightbox from "@modules/postLightbox";
-import buildPostSignature from "@modules/postSignature";
-import buildPostSponsor from "@modules/postSponsor";
-import buildPostCommentAvatars from "@modules/postCommentAvatars";
-import buildHljsLineNumber from "@modules/hljsLineNumber";
-import buildToolbar from "@modules/toolbar";
-import buildPersonBoard from "@modules/personBoard";
-import loader from "@modules/loader";
+    clearHtmlElement,
+    addHtmlElement,
+} from "./consts/tools";
+import buildGithubCorner from "./modules/githubCorner";
+import buildCustomHeader from "./modules/customHeader";
+import buildCustomFooter from "./modules/customFooter";
+import buildProfile from "./modules/profile";
+import buildPostCatalog from "./modules/postCatalog";
+import buildPostLightbox from "./modules/postLightbox";
+import buildPostSignature from "./modules/postSignature";
+import buildPostSponsor from "./modules/postSponsor";
+import buildPostCommentAvatars from "./modules/postCommentAvatars";
+import buildHljsLineNumber from "./modules/hljsLineNumber";
+import buildToolbar from "./modules/toolbar";
+import buildPersonBoard from "./modules/personBoard";
+import loader from "./modules/loader";
 
 class Silence {
     constructor() {
         $.extend(true, options, window.$silence);
-
-        clearElement();
         this.building();
     }
 
     building() {
+        clearHtmlElement();
         buildCustomHeader();
         buildCustomFooter();
         buildGithubCorner();
@@ -41,7 +40,7 @@ class Silence {
             buildPostSignature();
             buildHljsLineNumber();
             buildPostCommentAvatars();
-            addElement();
+            addHtmlElement();
         } else {
             showSidebar();
             buildPersonBoard();
