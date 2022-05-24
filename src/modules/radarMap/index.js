466787e0a9ea46ac2c9915a3ef85d0514f316c4e
shiramashiro <zhengrenfu@outlook.com>
2022-05-25 00:44:06 +0800

    雷达图可缩放，缩放时加载2秒

--- src/modules/radarMap/index.js
+++ src/modules/radarMap/index.js
@@ -4,24 +4,60 @@ import { themeColors, getTheme } from "../../consts/tools";
 
 let fillColor = "", strokeColor = "";
 
+const unfoldIcon = `
+<svg class="icon unfold-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+  <path fill="#A7A7A7" d="M818.393225 300.939003c12.824073-14.09502 34.658358-15.126512 48.752354-2.303462 14.09502 12.843516 15.126512 34.678824 2.302439 48.752354l-332.676845 364.835266c-12.844539 14.114462-34.659381 15.127536-48.753377 2.302439-0.815575-0.733711-1.588171-1.486864-2.302439-2.302439l-0.080841-0.078795-0.13917-0.13917L153.018046 347.388918c-12.824073-14.074553-11.791557-35.909861 2.302439-48.752354 14.09502-12.824073 35.930327-11.792581 48.753377 2.303462l307.168891 336.845795 307.149449-336.845795L818.393225 300.939003 818.393225 300.939003z">
+</svg>
+`;
+
+const foldIcon = `
+<svg class="icon fold-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+  <path fill="#A7A7A7" d="M818.393225 712.230324c12.824073 14.09502 34.658358 15.126512 48.752354 2.303462 14.09502-12.843516 15.126512-34.678824 2.302439-48.752354l-332.676845-364.835266c-12.844539-14.114462-34.659381-15.127536-48.753377-2.302439-0.815575 0.733711-1.588171 1.486864-2.302439 2.302439l-0.080841 0.078795-0.13917 0.13917L153.018046 665.780409c-12.824073 14.074553-11.791557 35.909861 2.302439 48.752354 14.09502 12.824073 35.930327 11.792581 48.753377-2.303462l307.168891-336.845795 307.149449 336.845795L818.393225 712.230324 818.393225 712.230324z">
+  </path>
+</svg>
+`;
+
+const freshIcon = `
+<svg class="fresh-icon" viewBox="0 0 1162 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+  <path fill="#A7A7A7" d="M1015.265248 720.230902h-8.677481a34.709923 34.709923 0 0 1-26.032442-52.064884 433.874038 433.874038 0 0 0-216.937019-538.003807 425.196557 425.196557 0 0 0-485.938922 86.774808 43.387404 43.387404 0 0 1-60.742365-60.742365A520.648845 520.648845 0 0 1 807.00571 52.064885 511.971364 511.971364 0 0 1 1058.652652 694.19846a43.387404 43.387404 0 0 1-43.387404 26.032442zM581.39121 1023.942729a538.003807 538.003807 0 0 1-225.614499-52.064885 520.648845 520.648845 0 0 1-260.324423-624.778614 43.387404 43.387404 0 0 1 60.742366-26.032442 43.387404 43.387404 0 0 1 26.032442 52.064884 433.874038 433.874038 0 0 0 216.937019 520.648845 425.196557 425.196557 0 0 0 485.938922-86.774807 43.387404 43.387404 0 1 1 60.742365 60.742365 485.938922 485.938922 0 0 1-364.454192 156.194654z"></path>
+  <path fill="#A7A7A7" d="M199.582057 494.616403c-17.354962 0-26.032442 0-34.709923-17.354962l-34.709923-52.064884-60.742365 52.064884a43.387404 43.387404 0 0 1-60.742365-8.67748 43.387404 43.387404 0 0 1 8.677481-60.742366l95.452288-78.097326c8.677481-8.677481 26.032442-17.354962 34.709923-8.677481s26.032442 8.677481 26.032442 17.354961L234.29198 433.874038c17.354962 17.354962 8.677481 43.387404-17.354961 60.742365zM1015.265248 746.263345h-8.677481l-26.032442-26.032443-52.064885-95.452288c-17.354962-17.354962-8.677481-43.387404 17.354962-60.742365a60.742365 60.742365 0 0 1 60.742365 17.354961l26.032442 60.742366 60.742366-52.064885a52.064885 52.064885 0 0 1 60.742365 8.677481c8.677481 17.354962 8.677481 52.064885-8.677481 60.742365l-104.129769 78.097327z"></path>
+</svg>
+`;
+
 function buildRadarMap() {
   fillColor = themeColors[getTheme()].color;
   strokeColor = themeColors[getTheme()].color2;
 
   $("#sidebar_search").after(`
     <!-- 技能雷达图 -->
-    <div class="tech-radar">
-      <h3 class="catListTitle">技能雷达</h3>
-      <div class="radar-wrap">
-        <canvas id="radar-map" width="200" height="200"></canvas>
-        <div id="radar-floating"></div>
+    <div class="personal-tech">
+      <h3 class="catListTitle">
+        <div class="cat-list-title-wrap">
+          <div class="title">技能雷达</div>
+          <div class="icon-wrap" data-is-fold="false">
+            ${ unfoldIcon }
+          </div>
+        </div>
+      </h3>
+      <div class="tech-radar">
+        <div class="radar-wrap-loading">
+          <div class="loading-wrap">
+            ${ freshIcon }
+            <div>loading...</div>
+          </div>
+        </div>
+        <div class="radar-wrap">
+          <canvas data-is-loaded="false" id="radar-map" width="200" height="200"></canvas>
+          <div id="radar-floating"></div>
+        </div>
       </div>
     </div>
   `);
 
-  let canvas = document.getElementById("radar-map");
-  let ctx = canvas.getContext("2d");
-  drawRadarMap(options.radarMap, 100, 100, ctx);
+  // let canvas = document.getElementById("radar-map");
+  // let ctx = canvas.getContext("2d");
+  // drawRadarMap(options.radarMap, 100, 100, ctx);
+  radarMapSwitch();
 }
 
 function drawPolygon(j, x, y, sides, radius, coords, ctx) {
@@ -67,19 +103,19 @@ function drawRadarMap(config, x, y, ctx) {
 /**
  * 连接里层以及外层之间所有多边形
  *
- * @param coords 所有多边形（层）的坐标轴
+ * @param axis 所有多边形（层）的坐标轴
  * @param x 多边形最里层的圆心 x 轴
  * @param y 多边形最里层的圆心 y 轴
  * @param config 配置项
  * @param ctx canvas 上下文
  */
-function drawStria(coords, x, y, config, ctx) {
-  let _coords = coords[coords.length - 1].coords;
+function drawStria(axis, x, y, config, ctx) {
+  let coords = axis[axis.length - 1].coords;
 
   for ( let i = 0; i < config.sides; i++ ) {
     ctx.beginPath();
     ctx.moveTo(x, y);
-    ctx.lineTo(_coords[i].x, _coords[i].y);
+    ctx.lineTo(coords[i].x, coords[i].y);
     ctx.closePath();
     ctx.stroke();
   }
@@ -96,7 +132,7 @@ function drawStria(coords, x, y, config, ctx) {
  * @param ctx canvas 上下文
  */
 function drawData(radius, coords, x, y, config, ctx) {
-  let decidedCoords = [];
+  let axis = [];
   let maxLayer = coords[coords.length - 1].coords;
 
   ctx.beginPath();
@@ -125,7 +161,7 @@ function drawData(radius, coords, x, y, config, ctx) {
     } else {
       ctx.lineTo(_x, _y);
     }
-    decidedCoords.push({ title: config.data[i].title, star: config.data[i].star, x: _x, y: _y });
+    axis.push({ title: config.data[i].title, star: config.data[i].star, x: _x, y: _y });
   }
 
   ctx.closePath();
@@ -136,22 +172,22 @@ function drawData(radius, coords, x, y, config, ctx) {
   ctx.fillStyle = fillColor;
   ctx.fill();
 
-  drawPoint(decidedCoords, ctx);
-  drawMovablePanel(decidedCoords);
+  drawPoint(axis, ctx);
+  drawMovablePanel(axis);
 }
 
 /**
  * 绘制数据覆盖区域峰值的点
  *
- * @param coords 所有多边形（层）的坐标轴
+ * @param axis 所有多边形（层）的坐标轴
  * @param ctx canvas 上下文
  */
-function drawPoint(coords, ctx) {
+function drawPoint(axis, ctx) {
   ctx.strokeStyle = "white";
 
-  for ( let i = 0; i < coords.length; i++ ) {
+  for ( let i = 0; i < axis.length; i++ ) {
     ctx.beginPath();
-    ctx.arc(coords[i].x, coords[i].y, 1, 0, Math.PI * 2);
+    ctx.arc(axis[i].x, axis[i].y, 1, 0, Math.PI * 2);
     ctx.closePath();
     ctx.stroke();
     ctx.fillStyle = "white";
@@ -177,16 +213,63 @@ function drawMovablePanel(coords) {
               "display": "block", "left": `${ e.offsetX }px`, "top": `${ e.offsetY }px`
             });
             $(cnp).empty().append(`
-                            <div class="tech">技术栈：${ v.title }</div>
-                            <div class="star">掌握程度：${ v.star } 颗星</div>
-                        `);
+              <div class="tech">技术栈：${ v.title }</div>
+              <div class="star">掌握程度：${ v.star } 颗星</div>
+            `);
           }
         });
       }, 50);
-    }, mouseleave: function (e) {
+    },
+    mouseleave: () => {
       $(cnp).css({ "display": "none" });
     }
   });
+}
+
+function strToBool(str) {
+  return str !== "false";
+}
+
+function boolToStr(bool) {
+  return bool === false ? "false" : "true";
+}
+
+function radarMapSwitch() {
+  $(".personal-tech > .catListTitle > .cat-list-title-wrap > .icon-wrap")
+    .on("click", function () {
+      let isFold = strToBool($(this)[0].dataset.isFold);
+      $(this).children().remove();
+      if ( isFold ) {
+        $(this).append(unfoldIcon);
+        $(".personal-tech > .tech-radar").css({
+          "display": "none"
+        });
+      } else {
+        $(this).append(foldIcon);
+        $(".personal-tech > .tech-radar").css({
+          "display": "block"
+        });
+
+        let radarMapDom = $("#radar-map");
+        let isLoaded = strToBool($(radarMapDom)[0].dataset.isLoaded);
+
+        if ( !isLoaded ) {
+          new Promise(function (resolve, reject) {
+            let canvas = document.getElementById("radar-map");
+            let ctx = canvas.getContext("2d");
+            setTimeout(function () {
+              $(this).children();
+              drawRadarMap(options.radarMap, 100, 100, ctx);
+              resolve();
+            }, 2000);
+          }).then(r => {
+            $(".tech-radar > .radar-wrap-loading").css({ "display": "none" });
+          });
+          $(radarMapDom)[0].dataset.isLoaded = boolToStr(!isLoaded);
+        }
+      }
+      $(this)[0].dataset.isFold = boolToStr(!isFold);
+    });
 }
 
 export default buildRadarMap;
