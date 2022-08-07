96e8e63f957e78c1c385530d1158f57f014a7fa1
shiramashiro <zhengrenfu@outlook.com>
2022-08-08 02:04:12 +0800

    [test: 02]打包测试天气预报小组件

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -4,20 +4,15 @@ import "./index.less";
 
 function createWeather() {
   axios
-    .get(`http://wthrcdn.etouch.cn/WeatherApi?city=${ options.weather.city }`)
-    .then((res) => {
-      let $weather = $(new DOMParser().parseFromString(res.data, "text/xml")).find("weather").first();
-      let highest = $weather.find("high").html().match("\\d+")[0];
-      let lowest = $weather.find("low").html().match("\\d+")[0];
-      let type = $weather.find("day > type").html();
-      let wind = $weather.find("day > fengxiang").html();
+    .get(`http://www.yiketianqi.com/free/day?appid=17593272&city=${ options.weather.city }&appsecret=5hMYdnuZ&unescape=1`)
+    .then(({ data }) => {
       let bluepoint = `
         <div class="weather">
           <div class="city">${ options.weather.city }</div>
           <div class="data">
-            <div class="temperature">${ lowest }°/${ highest }°</div>
-            <div class="type">${ type }</div>
-            <div class="wind">${ wind }</div>
+            <div class="temper">${ data.tem_night }°/${ data.tem_day }°</div>
+            <div class="type">${ data.win }</div>
+            <div class="wind">${ data.wea }</div>
           </div>
         </div>
       `;
