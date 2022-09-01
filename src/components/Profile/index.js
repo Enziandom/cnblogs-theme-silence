bd2634717488042dd29dd6e8a26621aee8fd896d
Enziandom <zhengrenfu@outlook.com>
2022-09-01 11:22:50 +0800

    :art: 修改判断头像状态的逻辑

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -4,31 +4,27 @@ import options from "../../config/options";
 function createProfile() {
   const avatar = options.avatar;
   const favicon = options.favicon;
-  const statusKey = options.statusKey;
-  const blogerStatus = {
+  const key = options.statusKey;
+  const status = {
     activity: { color: "rgba(66, 185, 131, 0.95)", desc: "活跃中" },
     busyness: { color: "rgba(255, 69, 0, 0.95)", desc: "忙碌中" },
     growing: { color: "rgba(240,230,140, 0.95)", desc: "升级中" },
     idle: { color: "rgba(112,128,144, 0.95)", desc: "摸鱼中" }
   };
 
-  let nowBlogerStatus = {};
+  if (avatar && key) {
+    let state = {};
 
-  if (avatar && statusKey) {
-    for (let key in blogerStatus) {
-      if (blogerStatus.hasOwnProperty(key)) {
-        if (statusKey === key) {
-          nowBlogerStatus = blogerStatus[key];
-        }
-      }
+    if (status.hasOwnProperty(key)) {
+      state = status[key];
     }
 
     $("#sideBarMain").prepend(`
       <div class="profile-avatar-wrap">
-        <img class="esa-profile-avatar" src="${avatar}" alt="profile-avatar"/>
+        <img class="enzia-profile-avatar" src="${avatar}" alt="profile-avatar"/>
         <div class="bloger-status">
-          <div class="bloger-status-icon" style="background-color: ${nowBlogerStatus.color}"></div>
-          <div class="bloger-status-desc">${nowBlogerStatus.desc}</div>
+          <div class="bloger-status-icon" style="background-color: ${state.color}"></div>
+          <div class="bloger-status-desc">${state.desc}</div>
         </div>
       </div>
     `);
