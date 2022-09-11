7a1f2444a3f4572316c9e3802ef67c629cd6eeab
Enziandom <zhengrenfu@outlook.com>
2022-09-11 17:53:45 +0800

    :art: 【2】测试图片居中

--- src/components/TextImage/index.js
+++ src/components/TextImage/index.js
@@ -3,11 +3,11 @@ import "./index.scss";
 function createTextImage() {
   $("#cnblogs_post_body img").each(function (index, el) {
     let imgDesc = $(el).attr("desc");
-    let $imgWrapper = $(`<div class="enzia-img"></div>`);
-    $(el).after($imgWrapper);
-    $($imgWrapper).append($imgWrapper);
+    let $imgBox = $(`<div class="enzia-img"></div>`);
+    $(el).after($imgBox);
+    $($imgBox).append(el);
     if (imgDesc) {
-      $($imgWrapper).append(`<p class="enzia-img-desc">${imgDesc}</p>`);
+      $($imgBox).append(`<p class="enzia-img-desc">${imgDesc}</p>`);
     }
   });
 }
