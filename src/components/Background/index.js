d87afb63cf076372dfc2b90e469f5c6526719fe8
Enziandom <zhengrenfu@outlook.com>
2022-09-22 17:18:19 +0800

    :sparkles: 取消背景透明的，使用高斯模糊

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,33 +1,17 @@
 import "./index.scss";
+import { setCss } from "../../utils/css-helper";
 import options from "../../config/options";
 
-function getRandomInt(max) {
+function randomUint(max) {
   return Math.floor(Math.random() * max);
-}
-
-function find(arr, _el) {
-  return !!arr.find(el => el === _el);
 }
 
 function createBackground() {
   let ops = options.backgroundOps;
 
   if (ops && ops.urls.length > 0) {
-    let ras = getRandomInt(ops.urls.length);
-
-    let $bgMatte = $(`
-      <img class="enzia-bg-matte" src="${ops.urls[ras]}" style="opacity: ${ops.opacity}" />
-    `);
-    if (ops.mainOpacity) {
-      $("#home").css({ opacity: ops.mainOpacity });
-    }
-    if (ops.objectFit) {
-      let hv = find(["cover", "contain", "fill", "inherit", "initial", "none", "revert", "scale-down", "unset"], ops.objectFit);
-      if (hv) {
-        $bgMatte.css({ "object-fit": ops.objectFit });
-      }
-    }
-    $("body").append($bgMatte);
+    let random = randomUint(ops.urls.length);
+    setCss([{ body: { "background-image": `url(${ops.urls[random]})`, "--bg-blur": `${ops.blur}px` } }, { "#home": { opacity: ops.mainOpacity } }]);
   }
 }
 
