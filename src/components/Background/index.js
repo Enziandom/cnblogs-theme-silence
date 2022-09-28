84861121c2c7000d3ed838b0330e7df92df51f0a
Enziandom <zhengrenfu@outlook.com>
2022-09-29 00:14:11 +0800

    :bug: 修复背景 urls 获取length错误的问题

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -13,7 +13,8 @@ function find(arr, _el) {
 function createBackground() {
   let ops = options.backgroundOps;
 
-  if (ops && ops.urls.length > 0 && ops.urls) {
+  if (ops && ops.urls) {
+    if (ops.urls.length > 0) return;
     let random = randomUint(ops.urls.length);
     let quaSynx = ["cover", "contain", "fill", "inherit", "initial", "none", "revert", "scale-down", "unset"];
     let isQua = find(quaSynx, ops.objectFit);
